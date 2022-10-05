3fe10363807c103a787df7bfbf888d21f06335bc
Leo Sosa <pocho131991@gmail.com>
2022-10-04 22:48:19 -0300

    se agregan archivos a repositorio

---
+++ scripts.js
@@ -0,0 +1,16 @@
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    console.log(data)
+    ev.target.appendChild(document.getElementById(data));
+    document.getElementById(data).style.width = "90px";
+    document.getElementById(data).style.height = "70px";
+}
